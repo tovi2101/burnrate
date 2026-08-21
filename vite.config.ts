14bbc882915671fa0796841368ca7f5fe3f517f3
Burnrate Contributors <burnrate@localhost>
2026-08-21 16:48:57 -0700

    fix: app launches

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   clearScreen: false,
-  server: { port: 5173, strictPort: true },
+  server: { port: 5173, strictPort: true, watch: { ignored: ["**/src-tauri/**"] } },
   envPrefix: ["VITE_", "TAURI_"],
 });
