cf193a309b3071fe7a90826f69b1d3a2b55abeee
Burnrate Contributors <burnrate@localhost>
2026-08-22 16:38:43 -0700

    chore: pin v1.3 release package hashes

--- packaging/homebrew/burnrate.rb
+++ packaging/homebrew/burnrate.rb
@@ -1,6 +1,6 @@
 cask "burnrate" do
   version "1.3.0"
-  sha256 :no_check
+  sha256 "0382a715111611a9bf459e1e0fe68f66eed879dea8bca67227f7ebc31c5e22c1"
 
   url "https://github.com/tovi2101/burnrate/releases/download/v#{version}/Burnrate_#{version}_universal.dmg"
   name "Burnrate"
